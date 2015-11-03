5a4d4094029374b303922db2c533df658710a5a5
Kyle Smith <smith.kyle1734@gmail.com>
2015-11-02 21:15:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var ChildBot = require('./idle.js');
+var ChildBot = require('./idle.js'); //change to steam-idlebot if not running from same directory
 
 var Bot = new ChildBot('username', 'password', { //add username and password
 	guardCode: '', //put steamguard code here
