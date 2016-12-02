74fa093892596b5b8740fd12a55784c3d8ff47aa
Arfian Adam <arfianadam@yahoo.com>
2016-12-02 22:01:34 +0700

    added missing comma and improve indentation

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ var ChildBot = require('./idle.js'); //change to steam-idlebot if not running fr
 
 var Bot = new ChildBot('username', 'password', { //add username and password
 	guardCode: '', //put steamguard code here
-	gamesPlayed: [440, 730] //put appids here, separated by a comma
-    personaState: 1 //personaState (online, away, etc)(number 0-5)
+	gamesPlayed: [440, 730], //put appids here, separated by a comma
+	personaState: 1 //personaState (online, away, etc)(number 0-5)
 });
 
 Bot.connect();
